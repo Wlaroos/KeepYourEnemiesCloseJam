82b362d698ef5aae2e03d7ca962ac4f65127b5ff
Mikey <mikeybess27@gmail.com>
2023-09-21 14:27:47 -0500

    Fixing Dash

--- Assets/Scripts/CharacterController2D.cs
+++ Assets/Scripts/CharacterController2D.cs
@@ -4,28 +4,35 @@ using UnityEngine;
 public class CharacterController2D : MonoBehaviour
 {
     [SerializeField] private float _moveSpeed = 10f;
-    [SerializeField] private float _dashDistance = 50f;
-    [SerializeField] private float _dashDuration = 0.2f;
+    [SerializeField] private float _dashDistance = 30f;
+    [SerializeField] private float _dashDuration = 0.1f;
     [SerializeField] private float _dashCooldown = 1f;
 
     private Rigidbody2D _rb;
+    private SpriteRenderer _sr;
+    
     private Vector2 _movement;
     private Vector2 _dashDirection;
+    
     private bool _isDashing = false;
     private bool _canDash = true;
+    
+    private Color _originalColor;
 
-    private void Start()
+    private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _sr = GetComponent<SpriteRenderer>();
+        _originalColor = _sr.color;
     }
 
     private void Update()
     {
         // Handle dashing
-        if (Input.GetButtonDown("Dash") && !_isDashing && _canDash)
-        {
-            StartCoroutine(Dash());
-        }
+            if (Input.GetButtonDown("Dash") && !_isDashing && _canDash)
+            {
+                StartCoroutine(Dash());
+            }
     }
 
     private void FixedUpdate()
@@ -58,45 +65,33 @@ public class CharacterController2D : MonoBehaviour
     private IEnumerator Dash()
     {
         _isDashing = true;
-        Vector2 originalPosition = _rb.position;
         float elapsedTime = 0f;
-
-        // Calculate the desired dash speed
-        float dashSpeed = _dashDistance / _dashDuration;
 
         while (elapsedTime < _dashDuration)
         {
             elapsedTime += Time.deltaTime;
-            Vector2 dashVelocity = _dashDirection * dashSpeed;
-
-            // Clamp the dash velocity to a maximum value (e.g., 20 units per second)
-            dashVelocity = ClampMag(dashVelocity, dashSpeed * 0.9f,-dashSpeed * 0.9f);
-
-            // Update the position based on the clamped dash velocity
-            _rb.MovePosition(_rb.position + dashVelocity * Time.deltaTime);
+            _rb.MovePosition(_rb.position + _dashDirection * (_dashDistance / _dashDuration) * Time.deltaTime);
             yield return null;
         }
-
-        _rb.MovePosition(originalPosition + _dashDirection * _dashDistance);
+        
         _isDashing = false;
 
         // Start cooldown for the next dash
         StartCoroutine(DashCooldown());
     }
 
-
     private IEnumerator DashCooldown()
     {
         _canDash = false;
-        yield return new WaitForSeconds(_dashCooldown);
-        _canDash = true;
-    }
 
-    private static Vector2 ClampMag(Vector2 v, float max, float min)
-    {
-        double sm = v.sqrMagnitude;
-        if(sm > (double)max * (double)max) return v.normalized * max;
-        else if(sm < (double)min * (double)min) return v.normalized * min;
-        return v;
+        // Flash the sprite red
+        _sr.color = Color.red;
+        
+        yield return new WaitForSeconds(_dashCooldown);
+
+        // Reset the sprite color to the original color
+        _sr.color = _originalColor;
+
+        _canDash = true;
     }
 }
