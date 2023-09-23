c641780f5ceaa5f6070528e3117358c634215876
Mikey <mikeybess27@gmail.com>
2023-09-23 01:51:47 -0500

    Deathmark PingPong Rotate

--- Assets/Scripts/Utility/RotateHoverUtil.cs
+++ Assets/Scripts/Utility/RotateHoverUtil.cs
@@ -1,59 +1,62 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class RotateHoverUtil : MonoBehaviour
 {
-    [SerializeField] private bool usesLocalTime = true;
-    private float localTime;
+    [SerializeField] private bool _usesLocalTime = true;
+    private float _localTime;
 
-    [SerializeField] private bool canRotate;
-    [SerializeField] private Vector3 degreesPerSecond = new Vector3(0f,0f,15f);
+    [SerializeField] private bool _canRotate;
+    [SerializeField] private bool _canRotateBackAndForth;
+    [SerializeField] private bool _canHover;
+    [SerializeField] private Vector3 _degreesPerSecond = new Vector3(0f, 0f, 15f);
+    
+    [SerializeField] private float _amplitude = 0.5f;
+    public float Amplitude => _amplitude;
+    [SerializeField] private float _frequency = 1f;
 
-    [SerializeField] private bool canHover;
-    // Amount the object will go up and down. Range of movement is (position - amplitude) to (position + amplitude)
-    [SerializeField] private float amplitude = 0.5f;
-    public float Amplitude => amplitude;
-    // Time needed for one full cycle
-    [SerializeField] private float frequency = 1f;
-
-    private Vector3 posOffset = new Vector3();
-    private Vector3 tempPos = new Vector3();
+    private Vector3 _posOffset = new Vector3();
+    private Vector3 _tempPos = new Vector3();
 
     private float _sine;
     public float Sine => _sine;
 
-    void Awake()
+    private void Awake()
     {
-        posOffset = transform.localPosition;
-        localTime = Random.Range(0,1000);
-        frequency = Random.Range(frequency, frequency+.25f);
+        _posOffset = transform.localPosition;
+        _localTime = Random.Range(0, 1000);
+        _frequency = Random.Range(_frequency, _frequency + 0.25f);
     }
 
-    void Update()
+    private void Update()
     {
-        if(usesLocalTime)
+        if (_usesLocalTime)
         {
-            localTime += Time.deltaTime;
+            _localTime += Time.deltaTime;
         }
         else
         {
-            localTime = Time.fixedTime;
+            _localTime = Time.fixedTime;
         }
 
-        if (canRotate)
+        if (_canRotateBackAndForth)
         {
-            transform.Rotate(Time.deltaTime * degreesPerSecond, Space.Self);
+            float rotationDirection = _canRotateBackAndForth && Mathf.PingPong(_localTime, 1f) < 0.5f ? -1 : 1;
+            transform.Rotate(Time.deltaTime * _degreesPerSecond * rotationDirection, Space.Self);
         }
-
-        if (canHover)
+        
+        if (_canRotate)
         {
-            tempPos = posOffset;
+            transform.Rotate(Time.deltaTime * _degreesPerSecond, Space.Self);
+        }
+        
+        if (_canHover)
+        {
+            _tempPos = _posOffset;
 
-            _sine = Mathf.Sin(localTime * Mathf.PI * frequency);
-            tempPos.y += (_sine * amplitude);
+            _sine = Mathf.Sin(_localTime * Mathf.PI * _frequency);
+            _tempPos.y += (_sine * _amplitude);
 
-            transform.localPosition = tempPos;
+            transform.localPosition = _tempPos;
         }
     }
 }
