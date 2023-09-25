b6f5e9699c086c003a5529f89d659847921a425e
Mikey <mikeybess27@gmail.com>
2023-09-25 02:04:05 -0500

    Level Name Intro

---
+++ Assets/Scripts/LevelFadeManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelFadeManager : MonoBehaviour
+{
+    [SerializeField] private Image _fadeImage;
+    [SerializeField] private float _fadeDelay = 2f;
+    [SerializeField] private float _fadeDuration = 1.5f;
+
+    private TMP_Text _levelName;
+
+    private void Awake()
+    {
+        _fadeImage = GetComponent<Image>(); 
+        _levelName = transform.GetChild(0).GetComponent<TMP_Text>();
+    }
+
+    void Start()
+    {
+        StartLevel();
+        _levelName.text = "[ " + SceneManager.GetActiveScene().name + " ]";
+    }
+
+    void StartLevel()
+    {
+        Time.timeScale = 0f; // Pause the game by setting time scale to zero
+        StartCoroutine(FadeIn());
+    }
+
+    IEnumerator FadeIn()
+    {
+        yield return new WaitForSecondsRealtime(_fadeDelay);
+        
+        float elapsedTime = 0f;
+        Color startColor = _fadeImage.color;
+        Color startColor2 = _levelName.color;
+        Color endColor = new Color(startColor.r, startColor.g, startColor.b, 0f); // Fully transparent
+
+        while (elapsedTime < _fadeDuration)
+        {
+            elapsedTime += Time.unscaledDeltaTime;
+            _levelName.color = Color.Lerp(startColor2, endColor, elapsedTime * 2.5f / _fadeDuration);
+            _fadeImage.color = Color.Lerp(startColor, endColor, elapsedTime / _fadeDuration);
+            yield return null;
+        }
+
+        _fadeImage.color = endColor;
+        Time.timeScale = 1f; // Set time scale back to 1 to start the level
+        
+        Destroy(gameObject);
+    }
+}
